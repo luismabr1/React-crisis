267d35b3b69bb8a82d9ae4146862450d193164b1
LuismaBR <lsbrt4@gmail.com>
2019-07-31 16:20:08 -0400

    modificacion de nombres en componentes y elementos

---
+++ src/pages/Committees.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles/Committees.css';
+import confLogo from '../images/LOGO-MOVENU-BLANCO.png';
+
+
+class Committees extends React.Component{
+   render(){
+       return <h1>Hola Mundo</h1>
+   } 
+}
+
+
+export default Committees;
